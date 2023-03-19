99533b21fa5a0bc2eaca36f18e4c4269b11535e6
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-03-18 21:20:47 -0500

    added event listener

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var city;
+var urlUnits = "https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}&units=metric";
 var weather = {
     "apiKey": "586ae82c4371851edd7b4bc796dc1a7b",
     fetchWeather: function(city) {
         // fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=586ae82c4371851edd7b4bc796dc1a7b")
-         fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
+         fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey + "&units=imperial")
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));
 
@@ -14,9 +15,21 @@ var weather = {
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
         console.log(name, icon, description, temp, humidity, speed);
-        document.querySelector("h2").innerText="Weather in " + name; 
+        document.querySelector(".city").innerText="Weather in " + name; 
+        document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png"; 
+        document.querySelector(".humidity").innerText="Humidity:" + humidity + "%"; 
+        document.querySelector(".wind").innerText=speed; 
+        document.querySelector(".description").innerText=description;
+        document.querySelector(".temperature").innerText= temp + "°" +"F"; 
 
-
+    },
+    searchCity : function() {
+        this.fetchWeather(document.querySelector(".searchBar").value)
     }
     
 };
+
+document.querySelector(".searchButton").addEventListener("click", function() {
+    weather.searchCity();
+
+})
