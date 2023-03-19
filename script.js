50112c276b4ef9c8e0c1f9d4167f74291cfaf0be
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-03-19 08:43:13 -0500

    added icons

--- script.js
+++ script.js
@@ -5,16 +5,16 @@
     const weather = {
         "apiKey": "586ae82c4371851edd7b4bc796dc1a7b",
         fetchWeather: function (city) {
-            fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey + "&units=imperial")
+            fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + this.apiKey + "&units=imperial")
                 .then((response) => response.json())
                 .then((data) => this.displayWeather(data));
 
         },
         displayWeather: function (data) {
-            const { name } = data;
-            const { icon, description } = data.weather[0];
-            const { temp, humidity } = data.main;
-            const { speed } = data.wind;
+            const { name } = data.city;
+            const { icon, description } = data.list[0].weather[0];
+            const { temp, humidity } = data.list[0].main;
+            const { speed } = data.list[0].wind;
             console.log(name, icon, description, temp, humidity, speed);
             document.querySelector(".city").innerText = "Weather in " + name;
             document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
@@ -23,6 +23,36 @@
             document.querySelector(".description").innerText = description;
             document.querySelector(".temperature").innerText = temp + "°" + "F";
 
+            let day1Ele =document.querySelector(".day-1");
+            day1Ele.querySelector('.dayDisplay').textContent=  dayjs().add(1, 'day').format("DD MMM YYYY");;
+            day1Ele.querySelector('.tempDisplay').textContent= "Temperature: "+ data.list[1].main.temp;
+            day1Ele.querySelector('.humidityDisplay').textContent= "Humidity: " +  data.list[1].main.humidity + "%";
+            day1Ele.querySelector('.descDisplay').textContent= "Description: "+ data.list[1].weather[0].description;
+            day1Ele.querySelector('.icon').src =  "https://openweathermap.org/img/wn/" +  data.list[1].weather[0].icon + "@2x.png";
+
+            let day2Ele =document.querySelector(".day-2");
+            day2Ele.querySelector('.dayDisplay').textContent=  dayjs().add(2, 'day').format("DD MMM YYYY");;
+            day2Ele.querySelector('.tempDisplay').textContent= "Temperature: "+ data.list[2].main.temp;
+            day2Ele.querySelector('.humidityDisplay').textContent= "Humidity: " +  data.list[2].main.humidity + "%";
+            day2Ele.querySelector('.descDisplay').textContent= "Description: "+ data.list[2].weather[0].description;
+            day2Ele.querySelector('.icon').src =  "https://openweathermap.org/img/wn/" +  data.list[2].weather[0].icon + "@2x.png";
+
+            let day3Ele =document.querySelector(".day-3");
+            day3Ele.querySelector('.dayDisplay').textContent=  dayjs().add(3, 'day').format("DD MMM YYYY");;
+            day3Ele.querySelector('.tempDisplay').textContent= "Temperature: "+ data.list[3].main.temp;
+            day3Ele.querySelector('.humidityDisplay').textContent= "Humidity: " +  data.list[3].main.humidity + "%";
+            day3Ele.querySelector('.descDisplay').textContent= "Description: "+ data.list[3].weather[0].description;
+            day3Ele.querySelector('.icon').src =  "https://openweathermap.org/img/wn/" +  data.list[3].weather[0].icon + "@2x.png";
+
+            let day4Ele =document.querySelector(".day-4");
+            day4Ele.querySelector('.dayDisplay').textContent=  dayjs().add(4, 'day').format("DD MMM YYYY");;
+            day4Ele.querySelector('.tempDisplay').textContent= "Temperature: "+ data.list[4].main.temp;
+            day4Ele.querySelector('.humidityDisplay').textContent= "Humidity: " +  data.list[4].main.humidity + "%";
+            day4Ele.querySelector('.descDisplay').textContent= "Description: "+ data.list[4].weather[0].description;
+            day4Ele.querySelector('.icon').src =  "https://openweathermap.org/img/wn/" +  data.list[4].weather[0].icon + "@2x.png";
+
+
+            
         },
         searchCity: function () {
             this.fetchWeather(document.querySelector(".searchBar").value)
@@ -38,9 +68,9 @@
 
     });
     document.querySelector(".currentDay").textContent =dayjs().format("DD MMM YYYY");
-    document.querySelector(".day-1").textContent = dayjs().add(1, 'day').format("DD MMM YYYY");
-    document.querySelector(".day-2").textContent = dayjs().add(2, 'day').format("DD MMM YYYY");
-    document.querySelector(".day-3").textContent = dayjs().add(3, 'day').format("DD MMM YYYY");
-    document.querySelector(".day-4").textContent = dayjs().add(4, 'day').format("DD MMM YYYY");
-    document.querySelector(".day-5").textContent = dayjs().add(5, 'day').format("DD MMM YYYY");
+   // document.querySelector(".day-1").textContent = dayjs().add(1, 'day').format("DD MMM YYYY");
+    //document.querySelector(".day-2").textContent = dayjs().add(2, 'day').format("DD MMM YYYY");
+    //document.querySelector(".day-3").textContent = dayjs().add(3, 'day').format("DD MMM YYYY");
+   // document.querySelector(".day-4").textContent = dayjs().add(4, 'day').format("DD MMM YYYY");
+   // document.querySelector(".day-5").textContent = dayjs().add(5, 'day').format("DD MMM YYYY");
 });
